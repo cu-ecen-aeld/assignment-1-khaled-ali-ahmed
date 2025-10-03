5e636b860a0308da4c3eef42179e5cb75ac755b1
khaled.ali.ahmed <khaled.ali.ahmed.habib@gmail.com>
2025-10-03 19:53:12 +0100

    first assignment

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -2,6 +2,9 @@
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
 
+ORIGINAL_DIR="$(pwd)"
+cd "$(dirname "$0")"
+
 set -e
 set -u
 
@@ -71,3 +74,5 @@ else
 	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
 	exit 1
 fi
+
+cd "$ORIGINAL_DIR"
